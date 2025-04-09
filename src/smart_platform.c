f496207fb6807f344aafbcdd72c949bfe6c283c3
PFiS1737 <pfis1737@gmail.com>
2025-04-09 20:36:47 +0800

    feat: place tabview to bottom

--- src/smart_platform.c
+++ src/smart_platform.c
@@ -5,10 +5,10 @@
 void smart_platform(void) {
     lv_theme_t *theme = lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED),
                                               false, &lv_font_han_sans_16_3500);
-    lv_disp_set_theme(NULL, theme);
+    lv_display_set_theme(NULL, theme);
 
-    lv_obj_t *tabview;
-    tabview = lv_tabview_create(lv_screen_active());
+    lv_obj_t *tabview = lv_tabview_create(lv_screen_active());
+    lv_tabview_set_tab_bar_position(tabview, LV_DIR_BOTTOM);
 
     lv_obj_t *tab1_home = lv_tabview_add_tab(tabview, "首页");
     lv_obj_t *tab2_hotpoints = lv_tabview_add_tab(tabview, "热点");
--- src/sp_home.c
+++ src/sp_home.c
@@ -1,13 +1,32 @@
 #include "sp_home.h"
 
 void init_textarea(lv_obj_t *parent);
+void init_material(lv_obj_t *parent);
 
 void sp_home(lv_obj_t *parent) {
     init_textarea(parent);
+}
 
-    lv_obj_t *label = lv_label_create(parent);
-    lv_label_set_text(label, "This is the home tab");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+// TODO: 这里应该是只把 home 里的输入栏作为占位符
+//       点击后，转跳到一个专门显示输入栏和搜索结果的页面
+//
+// TODO: (backend) 这里如果要做`关键词联想`，需要后端提供接口
+void ta_event_cb(lv_event_t *e);
+void init_textarea(lv_obj_t *parent) {
+    lv_obj_t *pinyin_ime = lv_ime_pinyin_create(lv_screen_active());
+
+    lv_obj_t *ta = lv_textarea_create(parent);
+    lv_textarea_set_one_line(ta, true);
+    lv_textarea_set_placeholder_text(ta, "请输入内容");
+    lv_obj_set_size(ta, LV_PCT(100), 50);
+    lv_obj_align(ta, LV_ALIGN_TOP_LEFT, 0, 0);
+
+    lv_obj_t *kb = lv_keyboard_create(lv_screen_active());
+    lv_ime_pinyin_set_keyboard(pinyin_ime, kb);
+    lv_keyboard_set_textarea(kb, ta);
+    lv_obj_add_flag(kb, LV_OBJ_FLAG_HIDDEN);
+
+    lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, pinyin_ime);
 }
 
 void ta_event_cb(lv_event_t *e) {
@@ -30,21 +49,4 @@ void ta_event_cb(lv_event_t *e) {
             break;
         default: break;
     }
-}
-
-void init_textarea(lv_obj_t *parent) {
-    lv_obj_t *pinyin_ime = lv_ime_pinyin_create(lv_screen_active());
-
-    lv_obj_t *ta = lv_textarea_create(parent);
-    lv_textarea_set_one_line(ta, true);
-    lv_textarea_set_placeholder_text(ta, "请输入内容");
-    lv_obj_set_size(ta, LV_PCT(100), 50);
-    lv_obj_align(ta, LV_ALIGN_TOP_LEFT, 0, 0);
-
-    lv_obj_t *kb = lv_keyboard_create(lv_screen_active());
-    lv_ime_pinyin_set_keyboard(pinyin_ime, kb);
-    lv_keyboard_set_textarea(kb, ta);
-    lv_obj_add_flag(kb, LV_OBJ_FLAG_HIDDEN);
-
-    lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, pinyin_ime);
 }
