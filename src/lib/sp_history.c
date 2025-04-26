f587fdba2b18481d158931a5a2ca0bc2b190b102
PFiS1737 <pfis1737@gmail.com>
2025-04-26 14:48:34 +0800

    feat: 为历史详情页面添加作文标题

--- src/lib/sp_history.c
+++ src/lib/sp_history.c
@@ -11,13 +11,22 @@ void sp_history(lv_obj_t *parent) {
     history_list = get_history_list();
     for(int i = 0; i < history_list->count; i++) {
         lv_obj_t *cont = lv_menu_cont_create(menu_main_page);
-        lv_obj_t *label = lv_label_create(cont);
 
-        lv_label_set_text_fmt(label, "%s  %s", i == 0 ? "（最新）" : "", history_list->list[i].title);
+        char *title = history_list->list[i].title;
+
+        lv_obj_t *label = lv_label_create(cont);
+        lv_label_set_text_fmt(label, "%s  %s", i == 0 ? "（最新）" : "", title);
         lv_obj_set_size(label, LV_PCT(100), LV_SIZE_CONTENT);
         lv_obj_set_style_text_font(label, &lv_font_han_sans_20_3500, 0);
 
-        lv_obj_t *content_page = lv_menu_page_create(menu, history_list->list[i].title);
+        lv_obj_t *content_page = lv_menu_page_create(menu, NULL);
+
+        lv_obj_t *content_title = lv_label_create(content_page);
+        lv_obj_set_size(content_title, LV_PCT(100), LV_SIZE_CONTENT);
+        lv_label_set_text_fmt(content_title, "%s\n", title);
+        lv_obj_set_style_text_font(content_title, &lv_font_han_sans_20_3500, 0);
+        lv_obj_set_style_text_align(content_title, LV_TEXT_ALIGN_CENTER, 0);
+
         lv_obj_t *content_label = lv_label_create(content_page);
         lv_obj_set_size(content_label, LV_PCT(100), LV_SIZE_CONTENT);
         lv_label_set_text(content_label, history_list->list[i].content);
